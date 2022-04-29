cbe8d8f8b57814b4de6564411cc62436c408b825
bantuvenkatesh123 <104574448+bantuvenkatesh123@users.noreply.github.com>
2022-04-29 09:16:30 +0530

    Delete data extraction.py

--- data extraction.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-import textwrap
-df=pd.read_csv('https://raw.githubusercontent.com/nethajinirmal13/Training-datasets/main/titanic_train.csv')
-df.sample(5)
